3ccbdb29cf792097218489c9ec44ce651003e88a
malhaq <mohammadabedalhaq02@gmail.com>
2024-01-08 09:01:25 +0200

    fgjgh

--- src/main/java/car/accessories/Main.java
+++ src/main/java/car/accessories/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 import java.util.logging.ConsoleHandler;
 
-public class Main // to delete
+public class Main
 {
 	static Scanner scan = new Scanner(System.in);
 	static Scanner str = new Scanner(System.in);
